8ef9e0f01adb3f39fb1580e20e81052f695ea2a0
Emanuel <108434968+EmanuelDeBrito@users.noreply.github.com>
2025-01-16 00:28:24 +0000

    Creating Hero Component

--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,11 +1,19 @@
 import { FilterArea } from "@/components/ui/filter-area"
 import { Header } from "@/components/ui/header"
+import { Hero } from "@/components/ui/hero"
 
 const Page = () => {
     return(
-        <div>
+        <div className="pb-7">
             <Header />
             <FilterArea />
+            <Hero 
+                title="Últimas novidades"
+                description="O que você vai assistir hoje?"
+                background="/one-punch-man.png"
+                backgroundTitle="ONE PUNCH MAN TERÁ 3 TEMPORADA"
+                backgroundDescription="Como relatado anteriormente, a franquia baseada no mangá escrito por ONE e ilustrado por Yusuke Murata, One Punch Man, divulgou um comunicado confirmando a produção de uma terceira temporada da adaptação para o anime […]"            
+            />
         </div>
     )
 }
